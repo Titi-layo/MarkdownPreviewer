aad56b7799a90ec96f6aeba3a2c9f7c067cbebfd
Titilayo Olorunsaiye <titilayoolorunsaiye@Titilayos-MBP.home>
2020-07-05 23:54:13 +0100

    Updates

---
+++ precache-manifest.97a17e43bb65972599809b1a918e9c7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9e85e2937e96aebeeffbe4b91ad2ad",
+    "url": "/Titi-layo/MarkdownPreviewer/index.html"
+  },
+  {
+    "revision": "7bb3f8607a0caae36695",
+    "url": "/Titi-layo/MarkdownPreviewer/static/css/main.54105c36.chunk.css"
+  },
+  {
+    "revision": "68920df4a23ebb38875b",
+    "url": "/Titi-layo/MarkdownPreviewer/static/js/2.7ac1629a.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/Titi-layo/MarkdownPreviewer/static/js/2.7ac1629a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb3f8607a0caae36695",
+    "url": "/Titi-layo/MarkdownPreviewer/static/js/main.db2c7bd2.chunk.js"
+  },
+  {
+    "revision": "57a51d426c838ca3a482",
+    "url": "/Titi-layo/MarkdownPreviewer/static/js/runtime-main.5f61cce2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Titi-layo/MarkdownPreviewer/static/media/logo.5d5d9eef.svg"
+  }
+]);
